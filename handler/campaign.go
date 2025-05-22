d060e33155dfbac7a2122f36764ee613a8418f63
mcahyogi <cahyogi288@gmail.com>
2025-05-22 21:56:46 +0700

    git campaigns endpoint object

--- handler/campaign.go
+++ handler/campaign.go
@@ -27,6 +27,6 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 		return
 	}
 
-	response := helper.APIResponse("List of campaign", http.StatusOK, "success", campaigns)
+	response := helper.APIResponse("List of campaign", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 }
